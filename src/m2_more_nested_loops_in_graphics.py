d874d9db1f0e0202c3f316aba8a64518eacd5a49
wegerac <wegerac@rose-hulman.edu>
2018-01-23 18:45:21 -0500

    Completed all TODOs

--- src/m2_more_nested_loops_in_graphics.py
+++ src/m2_more_nested_loops_in_graphics.py
@@ -3,8 +3,8 @@ This project demonstrates NESTED LOOPS (i.e., loops within loops)
 in the context of TWO-DIMENSIONAL GRAPHICS.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Andrew Weger.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -49,9 +49,34 @@ def draw_upside_down_wall(rectangle, n, window):
     and n is nonnegative.
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     Some tests are already written for you (above).
     # ------------------------------------------------------------------
+    movex = rectangle.get_upper_right_corner().x - rectangle.get_lower_left_corner().x
+    movey = rectangle.get_upper_right_corner().y - rectangle.get_lower_left_corner().y
+    p1 = rectangle.get_lower_left_corner()
+    p2 = rectangle.get_upper_right_corner()
+    rectangle.attach_to(window)
+    window.render()
+    for k in range(n):
+        for x in range(k + 1):
+            xmov = (x * movex)/2
+            ymov = x * movey
+            p1r = rg.Point(p1.x - xmov, p1.y + ymov)
+            p2r = rg.Point(p2.x - xmov, p2.y + ymov)
+            rect = rg.Rectangle(p1r,p2r)
+            rect.attach_to(window)
+            window.render()
+        np1 = rect.get_upper_right_corner()
+        np2 = rect.get_lower_left_corner()
+        for j in range(k):
+
+            np1.x  = np1.x + movex
+            np2.x = np2.x + movex
+            new = rg.Rectangle(np1, np2)
+            new.attach_to(window)
+            window.render()
+
 
 
 # ----------------------------------------------------------------------
